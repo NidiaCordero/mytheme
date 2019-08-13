ef5e1dd19c6cbd6a9fa4f4a761dd27a8dc6c14fe
Cordero Nidia <ruizcorderonidia@gmail.com>
2019-08-13 15:23:12 +0200

    banner changement description dynamique wordpress

--- templates/banner.php
+++ templates/banner.php
@@ -4,6 +4,6 @@
       <img class="img-fluid mb-5 d-block mx-auto" src="http://localhost:8888/wp-content/themes/montheme/img/profile.png" alt="">
       <h1 class="text-uppercase mb-0"><?php echo get_bloginfo('name'); ?></h1>
       <hr class="star-light">
-      <h2 class="font-weight-light mb-0">Web Developer - Graphic Artist - User Experience Designer</h2>
+      <h2 class="font-weight-light mb-0"><?php echo get_bloginfo('description'); ?></h2>
     </div>
   </header>
