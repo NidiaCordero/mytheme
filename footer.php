0abe83e7f81cad344fa144d402c8f308ea48ddcb
Cordero Nidia <ruizcorderonidia@gmail.com>
2019-08-18 19:02:29 +0200

    add menu au footer

--- footer.php
+++ footer.php
@@ -7,33 +7,15 @@
             </div>
             <div class="col-md-4 mb-5 mb-lg-0">
                 <h4 class="text-uppercase mb-4">Around the Web</h4>
-                <ul class="list-inline mb-0">
-                    <li class="list-inline-item">
-                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
-                            <i class="fab fa-fw fa-facebook-f"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
-                            <i class="fab fa-fw fa-google-plus-g"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
-                            <i class="fab fa-fw fa-twitter"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
-                            <i class="fab fa-fw fa-linkedin-in"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
-                            <i class="fab fa-fw fa-dribbble"></i>
-                        </a>
-                    </li>
-                </ul>
+                <?php 
+                    <li class="list-inline-item">	                // on remplace le menu en dur par un menu dynamique
+                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">	                wp_nav_menu([ // n'oubliez pas d'aller sur la documentation de cette fonction pour voir quels sont tous les paramêtres que l'on peut mettre et ce à quoi ils servent
+                            <i class="fab fa-fw fa-facebook-f"></i>	                    'menu_class' => 'list-inline mb-0',
+                        </a>	                    'theme_location' => 'social-network-footer',
+                    </li>	                    'container' => '',
+                    <li class="list-inline-item">	                    'fallback_cb' => false,
+                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">	                ]);;
+                            <i class="fab fa-fw fa-google-plus-g"></i>	                ?>
             </div>
             <div class="col-md-4">
             <?php dynamic_sidebar('footer-right'); ?>
