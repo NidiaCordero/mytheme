a2b65fc70c6551fb465b357c888fd248831a05b9
Cordero Nidia <ruizcorderonidia@gmail.com>
2019-08-18 17:14:48 +0200

    refactorinf pour la partie menu

--- functions.php
+++ functions.php
@@ -1,18 +1,10 @@
 <?php
 // Pour ne pas avoir de fichier contenant trop de ligne de code nous allons repartir le tout dans des fichiers spécifiques afin de rendre le tout plus lisible.
-* Fonction qui va ajouter des scripts dynamiquement afin que l'on puisse les inclures dans le thème avec wp_head() et wp_footer()	// Nous découvrons ici la function require_once() de php qui nous permet d'importer des fichiers
-*	// Nous découvrons également la fonction get_template_directory() qui renvoi le chemin du dossier du thème actif sur (à ne pas confondre avec get_template_directory_uri() qui renvoi une url) 
-* @return void	require_once(get_template_directory() . '/includes/enqueue-script.php');
-/**
- * Fonction qui ajoute un menu au thème.
- *
- * @return void
- */
-function register_main_menu()
-{
-  register_nav_menu('main-menu', 'Menu principal dans le header.');
-}
-add_action('after_setup_theme', 'register_main_menu');
+ //Fonction qui va ajouter des scripts dynamiquement afin que l'on puisse les inclures dans le thème avec wp_head() et wp_footer()	// Nous découvrons ici la function require_once() de php qui nous permet d'importer des fichiers
+	// Nous découvrons également la fonction get_template_directory() qui renvoi le chemin du dossier du thème actif sur (à ne pas confondre avec get_template_directory_uri() qui renvoi une url) 
+require_once(get_template_directory() . '/includes/enqueue-script.php');
+require_once(get_template_directory() . '/includes/menu.php');
+
  /**
  * Fonction qui ajoute des attributes au balise a des nav_menu
  *
